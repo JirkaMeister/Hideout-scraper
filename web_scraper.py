1ec2bbf02fa7787718706343ea7ec6c04d80b74d
Jiri Charamza <jicharamza@seznam.cz>
2025-07-17 19:12:36 +0200

    Scraping hideout requirements into objects

---
+++ web_scraper.py
@@ -0,0 +1,138 @@
+#	File name:	product_scraper.py
+#	Author:     Jiří Charamza
+#   Brief:      Web scraper for getting hideout zones requirments
+
+import httpx
+from bs4 import BeautifulSoup
+from bs4 import element
+import sys
+import time
+
+def create_id(name: str):
+    # Create a unique ID for the zone based on its name
+    return name.lower().replace(' ', '_').replace('\"', '')
+
+class HideoutZone:
+    def __init__(self, name):
+        self.id = create_id(name)
+        self.name = name
+        self.requirements = []
+    
+    def add_zone_level(self):
+        self.requirements.append([])
+
+    def add_requirement(self, requirement_li, level):
+        try:
+            # Adjust level to match the index in requirements list
+            level -= 1 
+            new_requirement = ZoneRequirement(
+                ZoneRequirement.get_name(requirement_li),
+                ZoneRequirement.get_level_or_quantity(requirement_li),
+                ZoneRequirement.get_type(requirement_li)
+            )
+            self.requirements[level].append(new_requirement)
+        except:
+            print(f"Error processing requirement: {requirement_li}", file=sys.stderr)
+
+    def print_zone_info(self):
+        print(f"Zone ID: {self.id}")
+        print(f"Zone Name: {self.name}")
+        print("Requirements:")
+        if len(self.requirements) > 0:
+            for level, reqs in enumerate(self.requirements):
+                print(f"  Level {level + 1}:")
+                for req in reqs:
+                    print(f"    - {req}")
+
+class ZoneRequirement:
+    def __init__(self, name, number, req_type):
+        self.id = create_id(name)
+        self.name = name
+        self.number = number
+        if req_type == None:
+            raise Exception("Invalid requirement type")
+        self.type = req_type
+    
+    def get_name(list_item):
+        # Name of the requirement is the text of the <a> tag
+        return list_item.find('a').text.strip() if list_item.find('a') else 'N/A'
+
+    def get_level_or_quantity(list_item):
+        # If the first element is a string - quantity of the item  
+        if type(list_item.contents[0]) == element.NavigableString:
+            return list_item.contents[0] if list_item.contents else 'N/A'
+        # Otherwise the last element is the level of trader/skill/zone
+        else:
+            return list_item.contents[-1] if list_item.contents else 'N/A'
+    
+    def get_type(list_item):
+        if list_item :
+            # Traders and skills start with <a> tag
+            if type(list_item.contents[0]) == element.Tag:
+                if 'LL' in list_item.contents[-1]:
+                    return 'traders'
+                elif 'Level' in list_item.contents[-1]:
+                    return 'skills'
+            # Items start with a number
+            elif list_item.contents[0][0].isnumeric():
+                return 'items'
+            # Zones start with 'Level'
+            elif 'Level' in list_item.contents[0]:
+                return 'zones'
+            else:
+                return None
+    
+    def __repr__(self):
+        if self.type == 'items':
+            return f"{self.number}x {self.name}"
+        return f"{self.name} {self.number}"
+
+
+def get_zone_tables(url: str):
+    response = httpx.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    parent_div = soup.find('div', class_='wds-tabber dealer-tabber')
+    zone_divs = parent_div.find_all('div', class_='wds-tab__content')
+    zone_tables = [zone_div.find('table', class_='wikitable') for zone_div in zone_divs]
+    return zone_tables
+
+def extract_zone_info(zone_table):
+    for table in zone_table:
+        # Name of the zone is the first row of the table
+        header_row = table.select('tr:first-child th')
+        zone_name = header_row[0].contents[0].strip()
+
+        zone = HideoutZone(zone_name)
+
+        # First table of requirements are in the third row of the table
+        requirements_levels = []
+        i = 0
+        while True:
+            requirement_list = table.select(f'tr:nth-child({i + 3}) td:first-of-type ul li')
+            if not requirement_list:
+                break
+            requirements_levels.append(requirement_list)
+            zone.add_zone_level()
+            i += 1
+
+        for level, requirements_list in enumerate(requirements_levels, start=1):
+            for requirement in requirements_list:
+                zone.add_requirement(requirement, level)
+
+        zone.print_zone_info()
+
+
+if __name__ == '__main__':
+    url = 'https://escapefromtarkov.fandom.com/wiki/Hideout'
+    retries = 0
+
+    # Retry up to 3 times in case of an error
+    while retries < 3:
+        try:
+            zones = get_zone_tables(url)
+            extract_zone_info(zones)
+            break
+        except Exception as e:
+            retries += 1
+            print(f'Error scraping: {e}', file=sys.stderr)
+            time.sleep(1)
